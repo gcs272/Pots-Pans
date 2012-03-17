a1154e603640980bbc0bfab54841b189623ff418
James Connor <jamesjconnorjr@yahoo.com>
2012-03-17 21:53:07 +0000

    Mongo db integration methods for alert and message

--- potsandpans/alert.py
+++ potsandpans/alert.py
@@ -1,4 +1,5 @@
 from potsandpans import geo
+from utility import get_mongodb_connection
 
 class Alert:
      def __init__(self, sender_number, body_text, timestamp):
@@ -10,9 +11,10 @@ class Alert:
           self.latitude = 0
           self.longitude = 0
      def handle(self):
-          # subscriber = self.db_driver.get_subscriber(self.sender_number)
+	  conn = get_mongodb_connection()
+          subscriber = conn.potsandpans.subscriber(self.sender_number)
 	  coords = geo.boundingBox(subscriber.latitude, subscriber.longitude, 10)
-          #subscriber_list = DBDriver.GetSubscribers_inBox(sms)
+          subscriber_list = conn.potsandpans.subscriber(coords[0], coords[1], coords[2], coords[3])
           for target in subscribers:
                 print "Dispatcher.send(" + str(target.number) + ", " + self.body_text + ")"
 		# save message received
--- potsandpans/message.py
+++ potsandpans/message.py
@@ -19,4 +19,13 @@ class Message:
 		if self.is_subscription():
 			return Subscription(self.number, self.message, self.received)
 		else:
-			return Alert(self.number, self.message, self.received)
+			return Alert(self.number, self.message, self.received
+	
+	def log_received(self, message_id, timestamp):
+		conn = get_mongodb_connection()
+		message = conn.potsandpans.message
+		self.message = message.select(message_id)
+		if self.message is not None:
+			log = conn.potsandpans.log
+			log.insert({"message_id" : message_id,
+					"timestamp" : timestamp})
