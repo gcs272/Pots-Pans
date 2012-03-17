b878b005900faf6d90849de22446ba3c07328bb2
Graham Smith <gcs272@gmail.com>
2012-03-17 18:14:26 -0400

    retab alert

--- potsandpans/alert.py
+++ potsandpans/alert.py
@@ -2,25 +2,28 @@ from potsandpans import geo
 from utility import get_mongodb_connection
 
 class Alert:
-     def __init__(self, sender_number, body_text, timestamp):
-          self.sender_number = sender_number
-          self.body_text = body_text
-          self.timestamp = timestamp
-     def parse(self):
-          # subsciber_coord = self.db_driver.get_subscriber_coord(self.send_number)
-          self.latitude = 0
-          self.longitude = 0
-     def handle(self):
-	  conn = get_mongodb_connection()
-          subscriber = conn.potsandpans.subscriber()
-	  sub = subscriber.select(self.sender_number)
-	  coords = geo.boundingBox(sub.latitude, sub.longitude, 10)
-          subscriber_list = subscriber.select(coords[0], coords[1], coords[2], coords[3])
-          for target in subscribers:
-                print "Dispatcher.send(" + str(target.number) + ", " + self.body_text + ")"
-		# save message received
-     def to_dictionary(self):
-          convert = {"sender_number" : self.sender_number,
-                     "body_text" : self.body_text}
-     def save(self):
-         pass
+	def __init__(self, sender_number, body_text, timestamp):
+		self.sender_number = sender_number
+		self.body_text = body_text
+		self.timestamp = timestamp
+	
+	def parse(self):
+		# subsciber_coord = self.db_driver.get_subscriber_coord(self.send_number)
+		self.latitude = 0
+		self.longitude = 0
+	
+	def handle(self):
+		conn = get_mongodb_connection()
+		subscriber = conn.potsandpans.subscriber()
+		sub = subscriber.select(self.sender_number)
+		coords = geo.boundingBox(sub.latitude, sub.longitude, 10)
+		subscriber_list = subscriber.select(coords[0], coords[1], coords[2], coords[3])
+		for target in subscribers:
+			print "Dispatcher.send(" + str(target.number) + ", " + self.body_text + ")"
+			# save message received
+
+	def to_dictionary(self):
+		convert = {"sender_number" : self.sender_number,"body_text" : self.body_text}
+
+	def save(self):
+		pass
