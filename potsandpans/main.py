c9a333ad2c63aeaf146c3c20693141aa415468a8
Graham Smith <gcs272@gmail.com>
2012-03-17 17:36:44 -0400

    return json resp status

--- potsandpans/main.py
+++ potsandpans/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask, redirect, url_for, render_template, request
+import json
 
 app = Flask(__name__)
 app.config.from_pyfile('/etc/potsandpans/main.cfg')
@@ -8,11 +9,12 @@ from potsandpans.message import Message
 
 @app.route('/sms', methods=['POST'])
 def sms():
-	print 'in sms handler'
 	number = request.form.get('From', None)
 	body = request.form.get('Body', None)
 
 	if number and body:
-		print 'got number and body'
 		message = Message(number, body)
 		message.get_subclass().handle()	# Handle the subscription or alert
+		return json.dumps({'status': 'ok'})
+	else:
+		return json.dumps({'status': 'error', 'message': 'invalid sms'})
